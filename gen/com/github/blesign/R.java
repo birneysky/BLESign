8cb5c9205e1147f996b2116fce5af8d5463051e2
birneysky <birneysky@163.com>
2016-01-06 22:46:48 +0800

    first add

--- gen/com/github/blesign/R.java
+++ gen/com/github/blesign/R.java
@@ -9,16 +9,498 @@ package com.github.blesign;
 
 public final class R {
     public static final class attr {
+        /** <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int border_color=0x7f010001;
+        /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int border_width=0x7f010000;
+    }
+    public static final class color {
+        /** 蜜色 
+         */
+        public static final int aliceblue=0x7f040039;
+        /** 亚麻色 
+         */
+        public static final int antiquewhite=0x7f04002f;
+        /** 中灰兰色 
+         */
+        public static final int aqua=0x7f04008d;
+        /** 粟色 
+         */
+        public static final int aquamarine=0x7f04006e;
+        /** 沙褐色 
+         */
+        public static final int azure=0x7f040037;
+        /** 烟白色 
+         */
+        public static final int beige=0x7f040034;
+        public static final int bg_clear=0x7f040007;
+        public static final int bg_item=0x7f040003;
+        public static final int bg_lost=0x7f040006;
+        public static final int bg_menu_question_item_child=0x7f04000c;
+        public static final int bg_title=0x7f040004;
+        public static final int bg_title_personal_info=0x7f040005;
+        /** 浅玫瑰色 
+         */
+        public static final int bisque=0x7f04001a;
+        public static final int black=0x7f040009;
+        /** 番木色 
+         */
+        public static final int blanchedalmond=0x7f040018;
+        /** 暗绿色 
+         */
+        public static final int blue=0x7f040098;
+        /** 暗红色 
+         */
+        public static final int blueviolet=0x7f040068;
+        /** 暗灰色 
+         */
+        public static final int brown=0x7f04005d;
+        /** 亮青色 
+         */
+        public static final int burlywood=0x7f040041;
+        /** 菊兰色 
+         */
+        public static final int cadetblue=0x7f04007c;
+        /** 碧绿色 
+         */
+        public static final int chartreuse=0x7f04006f;
+        /** 茶色 
+         */
+        public static final int chocolate=0x7f04004c;
+        public static final int connection_state=0x7f04000d;
+        /** 暗桔黄色 
+         */
+        public static final int coral=0x7f040024;
+        /** 中绿色
+         */
+        public static final int cornflowerblue=0x7f04007b;
+        /** 柠檬绸色 
+         */
+        public static final int cornsilk=0x7f040014;
+        /** 淡灰色 
+         */
+        public static final int crimson=0x7f040044;
+        /** 浅绿色 
+         */
+        public static final int cyan=0x7f04008e;
+        public static final int dark=0x7f040000;
+        /** 中兰色 
+         */
+        public static final int darkblue=0x7f04009a;
+        /** 深天蓝色 
+         */
+        public static final int darkcyan=0x7f040094;
+        /** 中粉紫色 
+         */
+        public static final int darkgoldenrod=0x7f040054;
+        /** 亮蓝色 
+         */
+        public static final int darkgray=0x7f04005b;
+        /** 绿色 
+         */
+        public static final int darkgreen=0x7f040097;
+        /** 暗灰色 
+         */
+        public static final int darkgrey=0x7f04005c;
+        /** 银色 
+         */
+        public static final int darkkhaki=0x7f040051;
+        /** 重褐色 
+         */
+        public static final int darkmagenta=0x7f040066;
+        /** 军兰色 
+         */
+        public static final int darkolivegreen=0x7f04007d;
+        /** 亮肉色 
+         */
+        public static final int darkorange=0x7f040023;
+        /** 赭色 
+         */
+        public static final int darkorchid=0x7f04005f;
+        /** 暗洋红 
+         */
+        public static final int darkred=0x7f040067;
+        /** 紫罗兰色 
+         */
+        public static final int darksalmon=0x7f04003e;
+        /** 亮绿色 
+         */
+        public static final int darkseagreen=0x7f040064;
+        /** 中绿宝石 
+         */
+        public static final int darkslateblue=0x7f040080;
+        /** 橙绿色 
+         */
+        public static final int darkslategray=0x7f040086;
+        /** 暗瓦灰色 
+         */
+        public static final int darkslategrey=0x7f040087;
+        /** 中春绿色 
+         */
+        public static final int darkturquoise=0x7f040092;
+        /** 苍绿色 
+         */
+        public static final int darkviolet=0x7f040061;
+        /** 红橙色 
+         */
+        public static final int deeppink=0x7f040028;
+        /** 暗宝石绿 
+         */
+        public static final int deepskyblue=0x7f040093;
+        /** 石蓝色 
+         */
+        public static final int dimgray=0x7f040078;
+        /** 暗灰色 
+         */
+        public static final int dimgrey=0x7f040079;
+        /** 亮海蓝色 
+         */
+        public static final int dodgerblue=0x7f04008b;
+        /** 暗金黄色 
+         */
+        public static final int firebrick=0x7f040055;
+        /** 雪白色 
+         */
+        public static final int floralwhite=0x7f040012;
+        /** 海绿色 
+         */
+        public static final int forestgreen=0x7f040089;
+        /** 深粉红色 
+         */
+        public static final int fuchsia=0x7f040029;
+        /** 洋李色 
+         */
+        public static final int gainsboro=0x7f040043;
+        /** 鲜肉色 
+         */
+        public static final int ghostwhite=0x7f040031;
+        /** 桃色 
+         */
+        public static final int gold=0x7f04001e;
+        /** 苍紫罗兰色
+
+         */
+        public static final int goldenrod=0x7f040046;
+        public static final int gray=0x7f040001;
+        /** 水鸭色 
+         */
+        public static final int green=0x7f040096;
+        /** 苍宝石绿 
+         */
+        public static final int greenyellow=0x7f040059;
+        /** 天蓝色 
+         */
+        public static final int honeydew=0x7f040038;
+        /** 珊瑚色 
+         */
+        public static final int hotpink=0x7f040025;
+        /** 秘鲁色 
+         */
+        public static final int indianred=0x7f04004e;
+        /** 暗橄榄绿 
+         */
+        public static final int indigo=0x7f04007e;
+        public static final int ivory=0x7f04000e;
+        /** 艾利斯兰 
+         */
+        public static final int khaki=0x7f04003a;
+        /** 暗肉色 
+         */
+        public static final int lavender=0x7f04003f;
+        /** 海贝色 
+         */
+        public static final int lavenderblush=0x7f040016;
+        /** 黄绿色 
+         */
+        public static final int lawngreen=0x7f040070;
+        /** 花白色 
+         */
+        public static final int lemonchiffon=0x7f040013;
+        public static final int light=0x7f040002;
+        /** 黄绿色 
+         */
+        public static final int lightblue=0x7f04005a;
+        /** 黄褐色 
+         */
+        public static final int lightcoral=0x7f04003b;
+        /** 淡紫色 
+         */
+        public static final int lightcyan=0x7f040040;
+        /** 老花色 
+         */
+        public static final int lightgoldenrodyellow=0x7f04002d;
+        /** 蓟色 
+         */
+        public static final int lightgray=0x7f040049;
+        /** 中紫色 
+         */
+        public static final int lightgreen=0x7f040063;
+        /** 亮灰色 
+         */
+        public static final int lightgrey=0x7f04004a;
+        /** 粉红色 
+         */
+        public static final int lightpink=0x7f040020;
+        /** 橙色 
+         */
+        public static final int lightsalmon=0x7f040022;
+        /** 森林绿 
+         */
+        public static final int lightseagreen=0x7f04008a;
+        /** 紫罗兰蓝色 
+         */
+        public static final int lightskyblue=0x7f040069;
+        /** 中暗蓝色
+         */
+        public static final int lightslategray=0x7f040072;
+        /** 亮蓝灰 
+         */
+        public static final int lightslategrey=0x7f040073;
+        /** 粉蓝色 
+         */
+        public static final int lightsteelblue=0x7f040057;
+        /** 象牙色 
+         */
+        public static final int lightyellow=0x7f04000f;
+        /** 春绿色 
+         */
+        public static final int lime=0x7f040090;
+        /** 中海蓝 
+         */
+        public static final int limegreen=0x7f040085;
+        /** 亮
+金黄色 
+         */
+        public static final int linen=0x7f04002e;
+        /** 紫红色 
+         */
+        public static final int magenta=0x7f04002a;
+        /** 紫色 
+         */
+        public static final int maroon=0x7f04006d;
+        /** 暗灰色 
+         */
+        public static final int mediumaquamarine=0x7f04007a;
+        /** 蓝色 
+         */
+        public static final int mediumblue=0x7f040099;
+        /** 褐玫瑰红 
+         */
+        public static final int mediumorchid=0x7f040053;
+        /** 暗紫罗兰色 
+         */
+        public static final int mediumpurple=0x7f040062;
+        /** 青绿色 
+         */
+        public static final int mediumseagreen=0x7f040084;
+        /** 草绿色 
+         */
+        public static final int mediumslateblue=0x7f040071;
+        /** 酸橙色 
+         */
+        public static final int mediumspringgreen=0x7f040091;
+        /** 靛青色 
+         */
+        public static final int mediumturquoise=0x7f04007f;
+        /** 印第安红 
+         */
+        public static final int mediumvioletred=0x7f04004f;
+        /** 闪兰色 
+         */
+        public static final int midnightblue=0x7f04008c;
+        /** 幽灵白 
+         */
+        public static final int mintcream=0x7f040032;
+        /** 白杏色 
+         */
+        public static final int mistyrose=0x7f040019;
+        /** 桔黄色 
+         */
+        public static final int moccasin=0x7f04001b;
+        /** 鹿皮色 
+         */
+        public static final int navajowhite=0x7f04001c;
+        /** 暗蓝色 
+         */
+        public static final int navy=0x7f04009b;
+        /** 红色 
+         */
+        public static final int oldlace=0x7f04002c;
+        /** 天蓝色 
+         */
+        public static final int olive=0x7f04006b;
+        /** 灰石色 
+         */
+        public static final int olivedrab=0x7f040076;
+        /** 亮粉红色 
+         */
+        public static final int orange=0x7f040021;
+        /** 西红柿色 
+         */
+        public static final int orangered=0x7f040027;
+        /** 金麒麟色 
+         */
+        public static final int orchid=0x7f040047;
+        /** 亮珊瑚色 
+         */
+        public static final int palegoldenrod=0x7f04003c;
+        /** 暗紫色 
+         */
+        public static final int palegreen=0x7f040060;
+        /** 亮钢兰色 
+         */
+        public static final int paleturquoise=0x7f040058;
+        /** 暗深红色 
+         */
+        public static final int palevioletred=0x7f040045;
+        /** 淡紫红 
+         */
+        public static final int papayawhip=0x7f040017;
+        /** 纳瓦白 
+         */
+        public static final int peachpuff=0x7f04001d;
+        /** 巧可力色 
+         */
+        public static final int peru=0x7f04004d;
+        /** 金色 
+         */
+        public static final int pink=0x7f04001f;
+        /** 实木色 
+         */
+        public static final int plum=0x7f040042;
+        /** 火砖色 
+         */
+        public static final int powderblue=0x7f040056;
+        /** 橄榄色 
+         */
+        public static final int purple=0x7f04006c;
+        /** 红紫色 
+         */
+        public static final int red=0x7f04002b;
+        /** 暗黄褐色 
+         */
+        public static final int rosybrown=0x7f040052;
+        /** 钢兰色 
+         */
+        public static final int royalblue=0x7f040082;
+        /** 暗海兰色 
+         */
+        public static final int saddlebrown=0x7f040065;
+        /** 古董白 
+         */
+        public static final int salmon=0x7f040030;
+        /** 浅黄色 
+         */
+        public static final int sandybrown=0x7f040036;
+        /** 暗瓦灰色 
+         */
+        public static final int seagreen=0x7f040088;
+        /** 米绸色 
+         */
+        public static final int seashell=0x7f040015;
+        /** 褐色 
+         */
+        public static final int sienna=0x7f04005e;
+        /** 中紫罗兰色 
+         */
+        public static final int silver=0x7f040050;
+        /** 亮天蓝色 
+         */
+        public static final int skyblue=0x7f04006a;
+        /** 深绿褐色 
+         */
+        public static final int slateblue=0x7f040077;
+        /** 亮蓝灰 
+         */
+        public static final int slategray=0x7f040074;
+        /** 灰石色 
+         */
+        public static final int slategrey=0x7f040075;
+        /** 黄色 
+         */
+        public static final int snow=0x7f040011;
+        /** 青色 
+         */
+        public static final int springgreen=0x7f04008f;
+        /** 暗灰蓝色 
+         */
+        public static final int steelblue=0x7f040081;
+        /** 亮灰色 
+         */
+        public static final int tan=0x7f04004b;
+        /** 暗青色 
+         */
+        public static final int teal=0x7f040095;
+        public static final int text_color_back_title_bar=0x7f04000b;
+        /** 淡紫色 
+         */
+        public static final int thistle=0x7f040048;
+        /** 热粉红色 
+         */
+        public static final int tomato=0x7f040026;
+        public static final int transparent=0x7f04000a;
+        /** 皇家蓝 
+         */
+        public static final int turquoise=0x7f040083;
+        /** 苍麒麟色 
+         */
+        public static final int violet=0x7f04003d;
+        /** 米色 
+         */
+        public static final int wheat=0x7f040035;
+        public static final int white=0x7f040008;
+        /** 薄荷色 
+         */
+        public static final int whitesmoke=0x7f040033;
+        /** 亮黄色 
+         */
+        public static final int yellow=0x7f040010;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int app_logo=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int listitem_add_device=0x7f020002;
+        public static final int lost_item_bg=0x7f020003;
+        public static final int no_tracker_item_bg=0x7f020004;
+        public static final int searching_item_bg=0x7f020005;
+        public static final int selected_item_bg=0x7f020006;
+        public static final int selected_tag=0x7f020007;
+        public static final int unselected_item_bg=0x7f020008;
+    }
+    public static final class id {
+        public static final int circleImageView_tracker_photo=0x7f070002;
+        public static final int circleImageView_tracker_state_bg=0x7f070006;
+        public static final int gridView_trackers=0x7f070000;
+        public static final int iv_no_tracker_item=0x7f070004;
+        public static final int iv_tracker_item_selected=0x7f070009;
+        public static final int layout_gridview_item=0x7f070001;
+        public static final int layout_state_tracker_item=0x7f070005;
+        public static final int layout_tracker_item_selected=0x7f070008;
+        public static final int tv_name_tracker_item=0x7f070007;
+        public static final int tv_state_tracker_item=0x7f070003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int gridview_item=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
     }
     public static final class style {
         /** 
@@ -42,10 +524,58 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a CircleImageView.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #CircleImageView_border_color com.github.blesign:border_color}</code></td><td></td></tr>
+           <tr><td><code>{@link #CircleImageView_border_width com.github.blesign:border_width}</code></td><td></td></tr>
+           </table>
+           @see #CircleImageView_border_color
+           @see #CircleImageView_border_width
+         */
+        public static final int[] CircleImageView = {
+            0x7f010000, 0x7f010001
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.github.blesign.R.attr#border_color}
+          attribute's value can be found in the {@link #CircleImageView} array.
+
+
+          <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.github.blesign:border_color
+        */
+        public static final int CircleImageView_border_color = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.github.blesign.R.attr#border_width}
+          attribute's value can be found in the {@link #CircleImageView} array.
+
+
+          <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.github.blesign:border_width
+        */
+        public static final int CircleImageView_border_width = 0;
+    };
 }
--- src/com/github/blesign/MainActivity.java
+++ src/com/github/blesign/MainActivity.java
@@ -1,15 +1,52 @@
 package com.github.blesign;
 
+import com.github.blesign.adapter.GridViewAdapter;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class MainActivity extends Activity {
+	//UI
+	private GridView gridViewTrackers;
+	private GridViewAdapter gvAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupView();
+        addListener();
     }
+
+	private void setupView() {
+		// TODO Auto-generated method stub
+		gridViewTrackers = (GridView)findViewById(R.id.gridView_trackers);
+	}
+
+	private void addListener() {
+		// TODO Auto-generated method stub
+		gridViewTrackers.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View arg1, int position, long arg3) {
+				if(position == (gvAdapter.getCount()-1)){//单击添加按钮
+					Intent intent = new Intent(MainActivity.this,AddBeaconActivity.class);
+					startActivityForResult(intent,AddBeaconActivity.REQUEST_ADD_IBEACON);
+					return;
+				}
+				//arg1是当前item的view，通过它可以获得该项中的各个组件。 arg2是当前item的ID。这个id根据你在适配器中的写法可以自己定义。arg3是当前的item在listView中的相对位置！
+				// 选中标识箭头显示,现在被挤压的看不见了
+				final GridViewAdapter.ViewHolder holder = (GridViewAdapter.ViewHolder) arg1.getTag();
+				if(holder.ivSelectedBg.getVisibility() == View.GONE){
+					holder.ivSelectedBg.setVisibility(View.VISIBLE);
+				}
+				
+			}
+		});
+	}
 }
